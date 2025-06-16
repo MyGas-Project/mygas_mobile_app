6f54df71347cfead645c85f9b8d840912d60d72f
harbi98 <janharvs98@gmail.com>
2025-06-16 09:56:52 +0800

    fixed: merged main to completed fixes

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import React, {createContext, useContext, useState} from 'react'
+const BASE_URL = "http://192.168.110.116:3000"
 
 const AuthContext = createContext();
 
@@ -6,7 +7,7 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({children}) => {
     const login = (email, password) => {
-        
+        // fetch();
     }
     const register = (email, password) => {
         
